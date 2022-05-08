4aaf471b5a22faf50e2a1e4adc7298d6fa40e75c
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-05-08 23:48:56 +0600

    nod

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='bg-info fixed' style={{marginBottom:'-2rem'}}>
-            <h3>All Rights Reserved By &copy;Car Manager</h3>
+            <h3>We provide best services in our inventory House - Car Manager</h3>
+            <h6>All Rights Reserved By &copy;Car Manager</h6>
         </div>
     );
 };
